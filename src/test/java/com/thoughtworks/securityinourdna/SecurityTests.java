aa2ab752a062359fc9d87c06b1962aa64224c783
rosatolen <rtolenti@thoughtworks.com>
2015-10-07 22:59:09 -0700

    adding different security test

--- src/test/java/com/thoughtworks/securityinourdna/SecurityTests.java
+++ src/test/java/com/thoughtworks/securityinourdna/SecurityTests.java
@@ -1,16 +1,25 @@
 package com.thoughtworks.securityinourdna;
 
-import org.apache.commons.io.IOUtils;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Form;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.NewCookie;
+import javax.ws.rs.core.Response;
 import java.io.IOException;
-import java.io.StringWriter;
 import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
+import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -18,24 +27,67 @@ import static org.junit.Assert.assertThat;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = VendorPortalApplication.class)
 @WebIntegrationTest()
+//@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class SecurityTests {
+    private final int HTTP_FORBIDDEN = 403;
+    private final int HTTP_OK = 200;
+
     @Test
-    public void shouldHaveCSRFProtection() throws IOException, InterruptedException {
+    public void admin_should_delete_user_with_csrf_token() throws Exception {
         // Given
-        URL userDeletionUrl = new URL("http://localhost:8080/vendor/delete/admin");
+        Client client = ClientBuilder.newClient();
+
+        URI loginUrl = new URI("http://localhost:8080/session");
+        Form form = new Form();
+        form.param("username", "admin");
+        form.param("password", "admin");
+        Map<String, NewCookie> cookieJar = client.target(loginUrl)
+                .request()
+                .post(Entity.form(form))
+                .getCookies();
+        String csrfCookieValue = cookieJar.get("csrf-token").getValue();
+        String sessionCookieValue = cookieJar.get("JSESSIONID").getValue();
+        URI userDeletionUrl = new URI("http://localhost:8080/vendor/delete/Recycling");
 
         // When
-        int status = makeRequest(userDeletionUrl, "POST").getResponseCode();
+        Form deletionFormParameters = new Form();
+        deletionFormParameters.param("csrf-token", csrfCookieValue);
+
+        Response deletionResponse = client.target(userDeletionUrl)
+                .request()
+                .cookie("JSESSIONID", sessionCookieValue)
+                .cookie("csrf-token", csrfCookieValue)
+                .post(Entity.form(deletionFormParameters));
 
         // Then
-        int httpStatusCodeForbidden = 403;
-        assertThat(status, is(httpStatusCodeForbidden));
+        int status = deletionResponse.getStatus();
+        assertThat(status, is(HTTP_OK));
     }
 
-    private HttpURLConnection makeRequest(URL url, String requestMethod) throws IOException {
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setDoOutput(true);
-        connection.setRequestMethod(requestMethod);
-        return connection;
+    @Test
+    public void sshouldHaveCSRFProtection() throws Exception {
+        // Given
+        Client client = ClientBuilder.newClient();
+
+        URI loginUrl = new URI("http://localhost:8080/session");
+        Form form = new Form();
+        form.param("username", "admin");
+        form.param("password", "admin");
+        Map<String, NewCookie> cookieJar = client.target(loginUrl)
+                .request()
+                .post(Entity.form(form))
+                .getCookies();
+        String sessionCookieValue = cookieJar.get("JSESSIONID").getValue();
+        URI userDeletionUrl = new URI("http://localhost:8080/vendor/delete/Electrician");
+
+        // When
+        Response deletionResponse = client.target(userDeletionUrl)
+                .request()
+                .cookie("JSESSIONID", sessionCookieValue)
+                .post(Entity.form(new Form()));
+
+        // Then
+        int status = deletionResponse.getStatus();
+        assertThat(status, is(HTTP_FORBIDDEN));
     }
 }
